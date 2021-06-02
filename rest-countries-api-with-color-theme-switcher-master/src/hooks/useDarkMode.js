706851f087416385332f0dffc33921e90e293020
Naoki Teguri <gurin140@gmail.com>
2021-06-03 02:09:00 +0900

    chore: remove hooks dir

--- rest-countries-api-with-color-theme-switcher-master/src/hooks/useDarkMode.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-
-const useDarkMode = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false)
-
-  return [isDarkMode, setIsDarkMode]
-}
-
-export default useDarkMode
