4ff58e832a8208c006d82306bccec963146400fb
Naoki Teguri <gurin140@gmail.com>
2021-06-04 00:46:45 +0900

    fix: add some styled-component stuff for server side rendring

--- rest-countries-api-with-color-theme-switcher-master/src/pages/_documents.js
+++ rest-countries-api-with-color-theme-switcher-master/src/pages/_documents.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -24,4 +25,35 @@ class MyDocument extends Document {
   }
 }
 
+MyDocument.getInitialProps = async (ctx) => {
+  const styledComponentSheet = new ServerStyleSheet();
+  const originalRenderPage = ctx.renderPage;
+
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          styledComponentSheet.collectStyles(
+            materialSheet.collect(<App {...props} />),
+          ),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [
+        <React.Fragment key="styles">
+          {initialProps.styles}
+          {styledComponentSheet.getStyleElement()}
+        </React.Fragment>,
+      ],
+    };
+  } finally {
+    styledComponentSheet.seal();
+  }
+};
+
+export default MyDocument;
 export default MyDocument;
